df0b97d608cb9d2311e5d24a294f09fbadeb2b0a
Fred Nicolls <30141109+nicollsf@users.noreply.github.com>
2026-02-14 11:07:06 +0200

    More autograder tests

--- test_locally.sh
+++ test_locally.sh
@@ -1,1 +1,1 @@
-gcc -std=c11 firmware/src/doa_algorithm.c firmware/tests/test_main.c -I firmware/include -o test_suite -lm && ./test_suite
+gcc -std=c11 firmware/src/doa_algorithm.c firmware/tests/test_doa.c -I firmware/include -o firmware/tests/test_doa -lm && ./firmware/tests/test_doa
